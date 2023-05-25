7d7a1d79e5ae9ed5e64be8f51dc0829f50c75420
VahidMhrb <m3hrabi@gmail.com>
2023-05-25 19:38:56 +0330

    add Strategy Pattern

---
+++ src/BehavioralPatterns/Strategy/ConcreteStrategyB.cs
@@ -0,0 +1,17 @@
+﻿namespace Strategy
+{
+    // Concrete Strategies implement the algorithm while following the base
+    // Strategy interface. The interface makes them interchangeable in the
+    // Context.
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+}
