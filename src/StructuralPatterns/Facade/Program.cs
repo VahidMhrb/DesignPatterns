904750841c2a86172e32f8b3ee896ccced10683f
VahidMhrb <m3hrabi@gmail.com>
2023-05-26 20:57:21 +0330

    add Facade Pattern

---
+++ src/StructuralPatterns/Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code may have some of the subsystem's objects already
+            // created. In this case, it might be worthwhile to initialize the
+            // Facade with these objects instead of letting the Facade create
+            // new instances.
+            Subsystem1 subsystem1 = new();
+            Subsystem2 subsystem2 = new();
+            Facade facade = new(subsystem1, subsystem2);
+            Client.ClientCode(facade);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/StructuralPatterns/Facade/Subsystems.cs
@@ -0,0 +1,32 @@
+﻿namespace Facade
+{
+    // The Subsystem can accept requests either from the facade or client
+    // directly. In any case, to the Subsystem, the Facade is yet another
+    // client, and it's not a part of the Subsystem.
+    public class Subsystem1
+    {
+        public string operation1()
+        {
+            return "Subsystem1: Ready!\n";
+        }
+
+        public string operationN()
+        {
+            return "Subsystem1: Go!\n";
+        }
+    }
+
+    // Some facades can work with multiple subsystems at the same time.
+    public class Subsystem2
+    {
+        public string operation1()
+        {
+            return "Subsystem2: Get ready!\n";
+        }
+
+        public string operationZ()
+        {
+            return "Subsystem2: Fire!\n";
+        }
+    }
+}
