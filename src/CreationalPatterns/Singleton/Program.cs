8818b868e7bd585710b5f670d650c61247ed5ab3
VahidMhrb <m3hrabi@gmail.com>
2023-05-27 09:54:50 +0330

    add Thread Safe Singleton Pattern

--- src/CreationalPatterns/Singleton/Program.cs
+++ src/CreationalPatterns/Singleton/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Non Thread Safe Singleton test");
+
             // The client code.
             Singleton s1 = Singleton.GetInstance();
             Singleton s2 = Singleton.GetInstance();
@@ -17,6 +19,37 @@
                 Console.WriteLine("Singleton failed, variables contain different instances.");
             }
             Console.ReadKey();
+
+            Console.WriteLine("Thread Safe Singleton test");
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+
+            Console.ReadKey();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            ThreadSafeSingleton singleton = ThreadSafeSingleton.GetInstance(value);
+            Console.WriteLine(singleton.Value);
         }
     }
 }
--- src/CreationalPatterns/Singleton/Singleton.cs
+++ src/CreationalPatterns/Singleton/Singleton.cs
@@ -38,4 +38,52 @@
             // ...
         }
     }
+
+
+    // This Singleton implementation is called "double check lock". It is safe
+    // in multithreaded environment and provides lazy initialization for the
+    // Singleton object.
+    class ThreadSafeSingleton
+    {
+        private ThreadSafeSingleton() { }
+
+        private static ThreadSafeSingleton _instance;
+
+        // We now have a lock object that will be used to synchronize threads
+        // during first access to the Singleton.
+        private static readonly object _lock = new object();
+
+        public static ThreadSafeSingleton GetInstance(string value)
+        {
+            // This conditional is needed to prevent threads stumbling over the
+            // lock once the instance is ready.
+            if (_instance == null)
+            {
+                // Now, imagine that the program has just been launched. Since
+                // there's no Singleton instance yet, multiple threads can
+                // simultaneously pass the previous conditional and reach this
+                // point almost at the same time. The first of them will acquire
+                // lock and will proceed further, while the rest will wait here.
+                lock (_lock)
+                {
+                    // The first thread to acquire the lock, reaches this
+                    // conditional, goes inside and creates the Singleton
+                    // instance. Once it leaves the lock block, a thread that
+                    // might have been waiting for the lock release may then
+                    // enter this section. But since the Singleton field is
+                    // already initialized, the thread won't create a new
+                    // object.
+                    if (_instance == null)
+                    {
+                        _instance = new ThreadSafeSingleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        // We'll use this property to prove that our Singleton really works.
+        public string Value { get; set; }
+    }
 }
