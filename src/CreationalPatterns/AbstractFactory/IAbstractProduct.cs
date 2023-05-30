2c40f04ade55e5d3693456ae3218972bc58359c1
VahidMhrb <m3hrabi@gmail.com>
2023-05-30 10:40:48 +0330

    Complete Abstract Factory

---
+++ src/CreationalPatterns/AbstractFactory/IAbstractProduct.cs
@@ -0,0 +1,77 @@
+﻿namespace AbstractFactory
+{
+    // Each distinct product of a product family should have a base interface.
+    // All variants of the product must implement this interface.
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+
+    // Concrete Products are created by corresponding Concrete Factories.
+    class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A1.";
+        }
+    }
+
+    class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A2.";
+        }
+    }
+
+    // Here's the the base interface of another product. All products can
+    // interact with each other, but proper interaction is possible only between
+    // products of the same concrete variant.
+    public interface IAbstractProductB
+    {
+        // Product B is able to do its own thing...
+        string UsefulFunctionB();
+
+        // ...but it also can collaborate with the ProductA.
+        //
+        // The Abstract Factory makes sure that all products it creates are of
+        // the same variant and thus, compatible.
+        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+
+    // Concrete Products are created by corresponding Concrete Factories.
+    class ConcreteProductB1 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B1.";
+        }
+
+        // The variant, Product B1, is only able to work correctly with the
+        // variant, Product A1. Nevertheless, it accepts any instance of
+        // AbstractProductA as an argument.
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B1 collaborating with the ({result})";
+        }
+    }
+    class ConcreteProductB2 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B2.";
+        }
+
+        // The variant, Product B2, is only able to work correctly with the
+        // variant, Product A2. Nevertheless, it accepts any instance of
+        // AbstractProductA as an argument.
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B2 collaborating with the ({result})";
+        }
+    }
+}
---
+++ src/CreationalPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+            Console.ReadKey();
+        }
+    }
+}
