2c844916f2746d8e5b31835d1e69d40253722301
VahidMhrb <m3hrabi@gmail.com>
2023-05-30 10:33:21 +0330

    add Iterator Pattern

---
+++ src/BehavioralPatterns/Iterator/IteratorAggregate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace Iterator
+{
+    abstract class IteratorAggregate : IEnumerable
+    {
+        // Returns an Iterator or another IteratorAggregate for the implementing
+        // object.
+        public abstract IEnumerator GetEnumerator();
+    }
+}
---
+++ src/BehavioralPatterns/Iterator/WordsCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Iterator
+{
+    // Concrete Collections provide one or several methods for retrieving fresh
+    // iterator instances, compatible with the collection class.
+    class WordsCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+
+        bool _direction = false;
+
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+
+        public List<string> getItems()
+        {
+            return _collection;
+        }
+
+        public void AddItem(string item)
+        {
+            _collection.Add(item);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this, _direction);
+        }
+    }
+}
