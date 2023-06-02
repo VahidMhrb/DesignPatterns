2d2a8a0ddbf0b4d1aad594b3b4eec378a396b875
VahidMhrb <m3hrabi@gmail.com>
2023-06-02 18:27:17 +0330

    Add Pattern Flyweight

---
+++ src/StructuralPatterns/Flyweight/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace Flyweight
+{
+    public class Car
+    {
+        public string Owner { get; set; } = string.Empty;
+
+        public string Number { get; set; } = string.Empty;
+
+        public string Company { get; set; } = string.Empty;
+
+        public string Model { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+    }
+}
---
+++ src/StructuralPatterns/Flyweight/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Flyweight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code usually creates a bunch of pre-populated
+            // flyweights in the initialization stage of the application.
+            var factory = new FlyweightFactory(
+                new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
+                new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
+                new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
+                new Car { Company = "BMW", Model = "M5", Color = "red" },
+                new Car { Company = "BMW", Model = "X6", Color = "white" }
+            );
+            factory.ListFlyweights();
+
+            AddCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "M5",
+                Color = "red"
+            });
+
+            AddCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "X1",
+                Color = "red"
+            });
+
+            factory.ListFlyweights();
+
+            Console.ReadKey();
+        }
+        public static void AddCarToPoliceDatabase(FlyweightFactory factory, Car car)
+        {
+            Console.WriteLine("\nClient: Adding a car to database.");
+
+            var flyweight = factory.GetFlyweight(new Car
+            {
+                Color = car.Color,
+                Model = car.Model,
+                Company = car.Company
+            });
+
+            // The client code either stores or calculates extrinsic state and
+            // passes it to the flyweight's methods.
+            flyweight?.Operation(car);
+        }
+    }
+}
