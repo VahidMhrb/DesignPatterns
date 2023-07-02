49077fdfb53ef38ad81af947536e2e9ec50f3d41
VahidMhrb <m3hrabi@gmail.com>
2023-07-02 16:14:45 +0330

    TemplateMethod add useage to Program.cs

--- src/BehavioralPatterns/TemplateMethod/Program.cs
+++ src/BehavioralPatterns/TemplateMethod/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Same client code can work with different subclasses:");
+
+            Client.ClientCode(new ConcreteClass1());
+
+            Console.Write("\n");
+
+            Console.WriteLine("Same client code can work with different subclasses:");
+            Client.ClientCode(new ConcreteClass2());
+            Console.ReadKey();
         }
     }
 }
