d0be8b09c0060a947ffe41a8adbd4bc8fd863a1f
VahidMhrb <m3hrabi@gmail.com>
2023-07-03 13:51:31 +0330

    Observer Pattern used

--- src/BehavioralPatterns/Observer/Program.cs
+++ src/BehavioralPatterns/Observer/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+            Console.ReadKey();
         }
     }
 }
