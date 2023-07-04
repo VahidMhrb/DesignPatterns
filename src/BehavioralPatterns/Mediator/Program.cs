f984635f24e043892559c9ff4b6968df7f1eb217
VahidMhrb <m3hrabi@gmail.com>
2023-07-04 08:20:25 +0330

    Mediator Pattern client Code

--- src/BehavioralPatterns/Mediator/Program.cs
+++ src/BehavioralPatterns/Mediator/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            // The client code.
+            Component1 component1 = new();
+            Component2 component2 = new();
+            new ConcreteMediator(component1, component2);
+
+            Console.WriteLine("Client triggers operation A.");
+            component1.DoA();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation D.");
+            component2.DoD();
+            Console.ReadKey();
         }
     }
 }
