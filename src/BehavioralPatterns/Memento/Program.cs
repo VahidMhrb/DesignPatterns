bc6275ca32472f899428f0016880881494002f5f
VahidMhrb <m3hrabi@gmail.com>
2023-07-05 23:45:49 +0330

    Memnto Patterm

--- src/BehavioralPatterns/Memento/Program.cs
+++ src/BehavioralPatterns/Memento/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            // Client code.
+            Originator originator = new Originator("Super-duper-super-puper-super.");
+            Caretaker caretaker = new Caretaker(originator);
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            Console.WriteLine();
+            caretaker.ShowHistory();
+
+            Console.WriteLine("\nClient: Now, let's rollback!\n");
+            caretaker.Undo();
+
+            Console.WriteLine("\n\nClient: Once more!\n");
+            caretaker.Undo();
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
